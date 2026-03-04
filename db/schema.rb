c067298be861408a1d977b095f12db3dc7c40e01
Ruby_on_Rails Developer <egorarbuzov@gmail.com>
2026-03-04 13:11:36 +0300

    corecting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_04_061900) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_04_100955) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,10 +40,11 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_04_061900) do
   end
 
   create_table "emails", force: :cascade do |t|
-    t.string "phone"
     t.string "email"
+    t.integer "phone_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["phone_id"], name: "index_emails_on_phone_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -90,5 +91,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_04_061900) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "emails", "phones"
   add_foreign_key "phones", "organizations"
 end
