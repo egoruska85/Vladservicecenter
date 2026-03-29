e31de53719712832b66f64b01cd0cac94cd5f512
egoruska85 <egorarbuzov@gmail.com>
2026-03-29 20:53:17 +0300

    dsadas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale, :set_variable
+  before_action :set_locale
 
   private
-
 
 
   def set_locale
